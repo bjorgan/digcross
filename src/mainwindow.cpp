2910f9c9b9ea747bfc41e689c601c214809f7b6c
Asgeir Bjørgan <asgeirbj@samfundet.no>
2016-09-13 19:39:21 +0200

    Test qlabel for displaying string from card reader.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -9,11 +9,15 @@
 MainWindow::MainWindow(QWidget *parent)
 {
 	QGridLayout *layout = new QGridLayout(this);
-	layout->addWidget(new QLabel("ARK kryssesystem"));
+	layout->addWidget(new QLabel("ARK kryssesystem. Bipp et kort:"));
+
+	QLabel *testLabel = new QLabel;
+	layout->addWidget(testLabel);
 
 	//install cardReader as an filter which intercepts keyboard events
 	CardReader *cardReader = new CardReader(this);
 	installEventFilter(cardReader);
+	connect(cardReader, SIGNAL(newCardNumber(QString)), testLabel, SLOT(setText(QString)));
 }
 
 bool CardReader::eventFilter(QObject *object, QEvent *event)
