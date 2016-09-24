03e75e964d4b16cd86ceb6399f3e7fee6684714d
Asgeir Bjørgan <asgeirbj@samfundet.no>
2016-09-24 16:06:52 +0200

    Removed dummy signal emmitance.

--- src/daemon_client.cpp
+++ src/daemon_client.cpp
@@ -6,6 +6,4 @@ DaemonClient::DaemonClient(QObject *parent) : QObject(parent)
 
 void DaemonClient::processTransaction(QString card_number, float amount)
 {
-	sleep(3);
-	transactionFeedback("Ragni", 0, UFS_UNAVAILABLE);
 }
