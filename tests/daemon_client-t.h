0095f245c9898edb4103534fff92649bee7fd549
Asgeir Bjørgan <asgeirbj@samfundet.no>
2016-10-10 18:57:25 +0200

    Update test documentation again.

--- tests/daemon_client-t.h
+++ tests/daemon_client-t.h
@@ -6,7 +6,9 @@ class DaemonClientTest : public QObject {
 		/**
 		 * Connect to transaction daemon and verify the feedback.  For
 		 * convenience in checking that the daemon and full dbus chain
-		 * is correctly set up (integration test).
+		 * is correctly set up (kind of integration test, will assume
+		 * that the daemon and associated configs are correctly
+		 * installed to system-wide paths).
 		 **/
 		void daemonConnection();
 };
