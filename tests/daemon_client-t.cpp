bfc096f30de1e27eb936e9a97cedbce898382bc8
Asgeir Bjørgan <asgeirbj@samfundet.no>
2016-10-13 19:49:42 +0200

    Rename daemon client test, add test for checking whether DBus service is available.

--- tests/daemon_client-t.cpp
+++ tests/daemon_client-t.cpp
@@ -3,9 +3,33 @@
 #include "daemon_client.h"
 #include <iostream>
 
+#include <QtDBus/QtDBus>
+#include "daemon_common.h"
+
 const int SIGNAL_TIMEOUT = 1000;
 
-void DaemonClientTest::daemonConnection()
+void DaemonClientTest::dbusServiceAvailable()
+{
+	//check DBus daemon availability
+	QDBusConnection connection = QDBusConnection::systemBus();
+	QVERIFY(connection.isConnected());
+
+	//check digcrossd service availability
+	QDBusMessage call = QDBusMessage::createMethodCall(DBUS_SERVICE_NAME, QLatin1String("/"), QLatin1String("org.freedesktop.DBus.Introspectable"), QLatin1String("Introspect"));
+	QDBusReply<QString> dbus_reply = connection.call(call);
+	if (!dbus_reply.isValid()) {
+		QDBusError err = dbus_reply.error();
+		if (err.type() == QDBusError::ServiceUnknown) {
+			//likely error: .service-file was not installed to /usr/share/dbus-1/system.d/, run make install
+			QVERIFY2(dbus_reply.isValid(), "digcrossd .service file is missing");
+		} else {
+			//likely error: .conf-file was not installed to /etc/dbus-1/system.conf, or digcrossd is missing from PATH, or digcrossd crashed on launch.
+			QVERIFY2(dbus_reply.isValid(), QString("digcrossd could not be executed by DBus, insufficient DBus policies, missing executable or daemon exited with error code. DBus error message: " + err.message()).toStdString().c_str());
+		}
+	}
+}
+
+void DaemonClientTest::sendTransactionRequestToDaemonAndWaitForFeedback()
 {
 	DaemonClient client;
 
--- tests/daemon_client-t.h
+++ tests/daemon_client-t.h
@@ -4,11 +4,16 @@ class DaemonClientTest : public QObject {
 	Q_OBJECT
 	private slots:
 		/**
+		 * Test that DBus service provided by the digcrossd daemon is available.
+		 **/
+		void dbusServiceAvailable();
+
+		/**
 		 * Connect to transaction daemon and verify the feedback.  For
 		 * convenience in checking that the daemon and full dbus chain
 		 * is correctly set up (kind of integration test, will assume
 		 * that the daemon and associated configs are correctly
 		 * installed to system-wide paths).
 		 **/
-		void daemonConnection();
+		void sendTransactionRequestToDaemonAndWaitForFeedback();
 };
