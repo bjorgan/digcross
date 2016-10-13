259fecfbdc373906448c09b47bcf7bb3acc4e56b
Øyvind Karlsen <oyvkar1@gmail.com>
2016-10-13 23:09:53 +0200

    Cleaned up return codes

--- src/userdatabase.h
+++ src/userdatabase.h
@@ -18,20 +18,11 @@ public:
 	 * Close the db connection
 	 */
 	~userDatabase();
-
-	/**
-	 * @brief Find username associated to card
-	 * @param CARD_ID - The card to investigate
-	 * @return "USER_ID" - username for user associated to card
-	 * 	   "CARD_NOT_FOUND" - error message for non-associated cards
-	 */
-	QString findUser(const QString& CARD_ID);
-
 	/**
 	 * @brief Create new row in database, populate with USER_ID
 	 * @param USER_ID - username of new user
 	 * @return 0 - SUCCESS - user successfully created
-	 *         1 - USER_EXISTS_ERROR
+	 *         1 - USER_EXISTS_ERROR - user already exists
 	 */
 	int createUser(const QString& USER_ID);
 
@@ -39,7 +30,7 @@ public:
 	 * @brief Delete user
 	 * @param USER_ID - username to delete
 	 * @return 0 - SUCCESS - User successfully deleted
-	 *  	   1 - USER_EXISTS_ERROR
+	 *  	   2 - NO_USER_ERROR - user does not exist
 	 */
 	int deleteUser(const QString& USER_ID);
 	/**
@@ -47,10 +38,20 @@ public:
 	 * @param USER_ID - username to change card info for
 	 * 	  CARD_ID - card info
 	 * @return 0 - SUCCESS - Card id for user successfully updated
-	 *         1 - USER_EXISTS_ERROR
-	 *
+	 *         2 - NO_USER_ERROR - user does not exist
+	 *         3 - CARD_OWNED_ERROR - card associated with different user
 	 */
 	int editCard(QString USER_ID, QString CARD_ID);
+
+	/**
+	 * @brief Find username associated to card
+	 * @param CARD_ID - The card to investigate
+	 *        result - Pointer which will be populated with username if SUCCESS
+	 * @return 0 - SUCCESS - username associated with card written to result
+	 * 	   4 - NO_USER_ERROR - No users associated with card, result populated with "error"
+	 */
+	int findUser(const QString& CARD_ID, const QString *result);
+
 private:
 	QSqlDatabase m_db;
 	
