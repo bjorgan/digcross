b6b2fb501598da7d420d2cd283e896221e9889cb
Asgeir Bjørgan <asgeirbj@samfundet.no>
2016-10-15 18:18:27 +0200

    Add documentation to all QAbstractTableModel-related functions in shoppinglist.

--- src/shoppinglist.h
+++ src/shoppinglist.h
@@ -27,12 +27,55 @@ class ShoppingList : public QAbstractTableModel {
 		double getTotalPrice();
 
 		/**
-		 * Neccessary functions for QAbstractTableModel subclassing.
+		 * \defgroup TableModelFunctions QAbstractTableModel subclassing
+		 * Functions related to the QAbstractTableModel subclassing.
+		 **/
+
+		/**
+		 * \ingroup TableModelFunctions
+		 * Number of rows in the table model represented by ShoppingList. Necessary for QAbstractTableModel subclassing.
+		 *
+		 * \param parent Parent index
+		 * \return Number of items in shopping list
 		 **/
 		virtual int rowCount(const QModelIndex &parent = QModelIndex()) const;
+
+		/**
+		 * \ingroup TableModelFunctions
+		 * Number of columns in the table model. Necessary for QAbstractTableModel subclassing.
+		 *
+		 * \param parent Parent index
+		 * \return Always 3 (name + price + amount)
+		 **/
 		virtual int columnCount(const QModelIndex &parent = QModelIndex()) const;
+
+		/**
+		 * \ingroup TableModelFunctions
+		 * Data access function. Necessary for QAbstractTableModel subclassing. Implements only Qt::DisplayRole.
+		 *
+		 * \param index Index in table. row is assumed to correspond to position in the shopping list, while columns 0, 1 and 2 correspond to name, price and amount (see also ITEM_NAME_COL etc. defined in shoppinglist.cpp)
+		 * \param role Role, see Qt docs for QAbstractItemModel
+		 * \return Name, price, amount or QVariant(QVariant::Invalid) depending on column index
+		 **/
 		virtual QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+
+		/**
+		 * \ingroup TableModelFunctions
+		 * Data setting function. Necessary for QAbstractTableModel subclassing. Implements only Qt::EditRole, allows editing only for the amount of an already inserted item.
+		 *
+		 * \param index Index in table, see documentation for data()
+		 * \param value New value for the specified table position
+		 * \param role Role
+		 **/
 		virtual bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole);
+
+		/**
+		 * \ingroup TableModelFunctions
+		 * Item flags for the defined table indices.
+		 *
+		 * \param index Index, see documentation for data()
+		 * \return Enabled and editable if the index corresponds to an amount for a shopping list item, disabled otherwise
+		 **/
 		virtual Qt::ItemFlags flags(const QModelIndex &index) const;
 	public slots:
 		/**
@@ -71,9 +114,20 @@ class ShoppingList : public QAbstractTableModel {
 		void deleteLastAddedItem();
 
 		/**
-		 * Convenience functions for QAbstractItemView-derived subclasses.
+		 * \ingroup TableModelFunctions
+		 * Convenience function for setting item amount from a QAbstractItemView-derived subclass.
+		 *
+		 * \param index Item index, see documentation for data()
+		 * \param amount New amount of item at the specified row-number
 		 **/
 		void setItemAmount(const QModelIndex &index, int amount);
+
+		/**
+		 * \ingroup TableModelFunctions
+		 * Convenience function for deleting an item from a QAbstractItemView-derived subclass.
+		 *
+		 * \param index Item index, see documentation for data()
+		 **/
 		void deleteItem(const QModelIndex &index);
 	private:
 		///List over shopping list entries, ordered by item name
