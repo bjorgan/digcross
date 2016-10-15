e89aea1a99c9d36fbb61835a51aea251570fd843
Asgeir Bjørgan <asgeirbj@samfundet.no>
2016-10-15 09:49:48 +0200

    Move testcase data to init()

--- tests/shoppinglist-t.cpp
+++ tests/shoppinglist-t.cpp
@@ -3,11 +3,20 @@
 #include "shoppinglist.h"
 #include <QTableView>
 
+const QString ITEM_1 = "test_item_1";
+const QString ITEM_2 = "test_item_2";
+const QString ITEM_3 = "test_item_3";
+const double PRICE = 20.0;
+const int AMOUNT = 20;
+
 void ShoppingListTest::init()
 {
 	shoppingList = new ShoppingList;
 
-	//TODO: Since all tests mostly use the same data, populate the shoppingList here.
+	//add test data
+	shoppingList->newItem(ITEM_1, PRICE, AMOUNT);
+	shoppingList->newItem(ITEM_2, PRICE, AMOUNT);
+	shoppingList->newItem(ITEM_3, PRICE, AMOUNT);
 }
 
 void ShoppingListTest::cleanup()
@@ -17,6 +26,10 @@ void ShoppingListTest::cleanup()
 
 void ShoppingListTest::testNewItem()
 {
+	//start from scratch (ignore test data created in init()
+	delete shoppingList;
+	shoppingList = new ShoppingList;
+
 	QString item_1 = "test_item";
 	QString item_2 = "test_item_2";
 
@@ -49,44 +62,30 @@ void ShoppingListTest::testNewItem()
 
 void ShoppingListTest::testSetItemAmount()
 {
-	int amount = 20;
-	shoppingList->newItem("test_item_1", 20.0, amount);
-	shoppingList->newItem("test_item_2", 20.0, amount);
-	shoppingList->newItem("test_item_3", 20.0, amount);
-
-	QCOMPARE(shoppingList->data(shoppingList->index(2, 2)).toInt(), amount);
+	QCOMPARE(shoppingList->data(shoppingList->index(2, 2)).toInt(), AMOUNT);
 
 	//check that the amount is changed when requested
-	shoppingList->setItemAmount("test_item_3", amount*2);
-	QCOMPARE(shoppingList->data(shoppingList->index(2, 2)).toInt(), amount*2);
+	shoppingList->setItemAmount(ITEM_3, AMOUNT*2);
+	QCOMPARE(shoppingList->data(shoppingList->index(2, 2)).toInt(), AMOUNT*2);
 }
 
 void ShoppingListTest::testDeleteItem()
 {
-	int amount = 20;
-	shoppingList->newItem("test_item_1", 20.0, amount);
-	shoppingList->newItem("test_item_2", 20.0, amount);
-	shoppingList->newItem("test_item_3", 20.0, amount);
-
 	QCOMPARE(shoppingList->rowCount(), 3);
 
 	//check that correct row is deleted when using the item name
-	shoppingList->deleteItem("test_item_2");
+	shoppingList->deleteItem(ITEM_2);
 	QCOMPARE(shoppingList->rowCount(), 2);
-	QCOMPARE(shoppingList->data(shoppingList->index(0, 0)).toString(), QString("test_item_1"));
-	QCOMPARE(shoppingList->data(shoppingList->index(1, 0)).toString(), QString("test_item_3"));
+	QCOMPARE(shoppingList->data(shoppingList->index(0, 0)).toString(), ITEM_1);
+	QCOMPARE(shoppingList->data(shoppingList->index(1, 0)).toString(), ITEM_3);
 
 	//check that the correct row is deleted when using the row number
 	shoppingList->deleteItem(shoppingList->index(0, 0));
-	QCOMPARE(shoppingList->data(shoppingList->index(0, 0)).toString(), QString("test_item_3"));
+	QCOMPARE(shoppingList->data(shoppingList->index(0, 0)).toString(), ITEM_3);
 }
 
 void ShoppingListTest::testWipeList()
 {
-	int amount = 20;
-	shoppingList->newItem("test_item_1", 20.0, amount);
-	shoppingList->newItem("test_item_2", 20.0, amount);
-	shoppingList->newItem("test_item_3", 20.0, amount);
 	QCOMPARE(shoppingList->rowCount(), 3);
 
 	//verify that list is wiped
@@ -99,29 +98,29 @@ void ShoppingListTest::testWipeList()
 
 void ShoppingListTest::testDeleteLastItem()
 {
-	int amount = 20;
-	shoppingList->newItem("test_item_1", 20.0, amount);
-	shoppingList->newItem("test_item_2", 20.0, amount);
-	shoppingList->newItem("test_item_3", 20.0, amount);
-	QCOMPARE(shoppingList->rowCount(), 3);
+	shoppingList->newItem("something_different", 20, 20);
+	QCOMPARE(shoppingList->rowCount(), 4);
 
 	//verify that last item was deleted
 	shoppingList->deleteLastAddedItem();
-	QCOMPARE(shoppingList->rowCount(), 2);
-	QCOMPARE(shoppingList->data(shoppingList->index(0, 0)).toString(), QString("test_item_1"));
-	QCOMPARE(shoppingList->data(shoppingList->index(1, 0)).toString(), QString("test_item_2"));
+	QCOMPARE(shoppingList->rowCount(), 3);
+	QCOMPARE(shoppingList->data(shoppingList->index(0, 0)).toString(), ITEM_1);
+	QCOMPARE(shoppingList->data(shoppingList->index(1, 0)).toString(), ITEM_2);
+	QCOMPARE(shoppingList->data(shoppingList->index(2, 0)).toString(), ITEM_3);
 }
 
 void ShoppingListTest::testGetTotalAmount()
 {
-	QCOMPARE(shoppingList->getTotalAmount(), 0.0);
+	shoppingList->wipeList();
+	QCOMPARE(shoppingList->rowCount(), 0);
+
 	int amount = 20;
 	double price = 20;
-	shoppingList->newItem("test_item_1", price, amount);
+	shoppingList->newItem(ITEM_1, price, amount);
 	QCOMPARE(shoppingList->getTotalAmount(), price*amount*1.0);
-	shoppingList->newItem("test_item_2", price, amount);
+	shoppingList->newItem(ITEM_2, price, amount);
 	QCOMPARE(shoppingList->getTotalAmount(), 2*price*amount*1.0);
-	shoppingList->newItem("test_item_3", price, amount);
+	shoppingList->newItem(ITEM_3, price, amount);
 	QCOMPARE(shoppingList->getTotalAmount(), 3*price*amount*1.0);
 }
 
